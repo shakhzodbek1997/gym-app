feeabd866da1c5136a0aa2daaa5d4ad64ec0bbc8
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-08-14 12:52:37 +0200

    bugs are fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box
+      mt="80px"
+      bgcolor="#fff3f4" 
+    >
+      <Stack 
+        gap="40px"
+        alignItems="center"
+        px="40px"
+        pt="24px"
+      >
+        <img src={Logo} alt="Logo" width="200px" height="40px" />
+        <Typography
+          variant="h5"
+          pb="40px"
+          mt="20px"
+        >
+          Made With ❤️ by Shakhzodbek
+        </Typography>
+         
+      </Stack>
+    </Box>
   )
 }
 
